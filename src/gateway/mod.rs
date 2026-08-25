426be56b4cafc299a444753ccd63467e5db421ed
cybellereaper <79605349+cybellereaper@users.noreply.github.com>
2026-08-25 13:29:20 -0500

    Expose shard management

--- src/gateway/mod.rs
+++ src/gateway/mod.rs
@@ -3,11 +3,15 @@
 mod close;
 mod connection;
 mod event;
+mod identify;
 mod intents;
+mod rate_limit;
 mod session;
+mod shard;
 
 pub use close::{GatewayCloseCode, GatewayReconnectStrategy};
 pub use connection::{GatewayConfig, GatewayConnection};
 pub use event::{DispatchEvent, GatewayEvent};
 pub use intents::GatewayIntents;
 pub use session::GatewaySession;
+pub use shard::{ShardCount, ShardEvent, ShardId, ShardManager, shard_for_guild};
