cee954bc37c16c58218cbab9ecffd18f1d60e505
cybellereaper <79605349+cybellereaper@users.noreply.github.com>
2026-08-25 17:06:07 -0500

    Export message REST and upload APIs

--- src/http/mod.rs
+++ src/http/mod.rs
@@ -1,11 +1,21 @@
 //! Discord HTTP API support.
 
 mod client;
+mod encoding;
+mod message;
 mod models;
+mod pagination;
 mod rate_limit;
 mod response;
 mod route;
+mod upload;
 
 pub use client::{RestClient, RestClientBuilder};
+pub use message::{
+    ChannelPinsQuery, MessageListQuery, MessageSearchIndexing, MessageSearchQuery,
+    MessageSearchResponse, MessageSearchResult, ReactionUsersQuery,
+};
 pub use models::{GatewayBot, SessionStartLimit};
+pub use pagination::Pagination;
 pub use response::HttpResponse;
+pub use upload::{UploadFile, UploadSource};
