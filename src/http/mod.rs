a76184169e5ca76a84910817aef8b3bc5bd33fd4
cybellereaper <79605349+cybellereaper@users.noreply.github.com>
2026-08-25 16:51:20 -0500

    Export REST client builder

--- src/http/mod.rs
+++ src/http/mod.rs
@@ -6,6 +6,6 @@ mod rate_limit;
 mod response;
 mod route;
 
-pub use client::RestClient;
+pub use client::{RestClient, RestClientBuilder};
 pub use models::{GatewayBot, SessionStartLimit};
 pub use response::HttpResponse;
