375622adc1916abd7dbd339c5afecbc980971751
cybellereaper <79605349+cybellereaper@users.noreply.github.com>
2026-08-25 13:18:54 -0500

    Expose Gateway lifecycle types

--- src/gateway/mod.rs
+++ src/gateway/mod.rs
@@ -1,9 +1,13 @@
 //! Discord Gateway WebSocket support.
 
+mod close;
 mod connection;
 mod event;
 mod intents;
+mod session;
 
+pub use close::{GatewayCloseCode, GatewayReconnectStrategy};
 pub use connection::{GatewayConfig, GatewayConnection};
 pub use event::{DispatchEvent, GatewayEvent};
 pub use intents::GatewayIntents;
+pub use session::GatewaySession;
