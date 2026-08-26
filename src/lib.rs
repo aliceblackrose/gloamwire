3a15f65b2101f7f7eda55555c6fbdc8176eb78f0
cybellereaper <79605349+cybellereaper@users.noreply.github.com>
2026-08-25 21:39:16 -0500

    feat: export OAuth2 and CDN APIs

--- src/lib.rs
+++ src/lib.rs
@@ -7,10 +7,20 @@
 
 #![deny(unsafe_code)]
 
+pub mod cdn;
 pub mod error;
 pub mod gateway;
 pub mod http;
 pub mod model;
+pub mod oauth2;
 
-pub use error::{DiscordApiError, DiscordValidationError, Error, Result};
+pub use cdn::{
+    Cdn, CdnAnimatedImageFormat, CdnAnimatedImageOptions, CdnImageFormat, CdnImageOptions,
+    CdnImageSize, CdnStickerFormat, InvalidCdnImageSize,
+};
+pub use error::{DiscordApiError, DiscordValidationError, Error, OAuth2ApiError, Result};
 pub use http::{Pagination, RestClient, RestClientBuilder, UploadFile, UploadSource};
+pub use oauth2::{
+    OAuth2Application, OAuth2AuthorizationInfo, OAuth2AuthorizationUrl, OAuth2Client, OAuth2Prompt,
+    OAuth2ResponseType, OAuth2TokenResponse, OAuth2TokenTypeHint,
+};
